008c5b8d1e46fce2cc87f21dbf01f39e8505dce3
이요한 <icb1696@naver.com>
2025-03-23 22:43:05 +0900

    d

--- src/main/java/team7/hrbank/domain/department/dto/DepartmentRequestDTO.java
+++ src/main/java/team7/hrbank/domain/department/dto/DepartmentRequestDTO.java
@@ -2,6 +2,7 @@ package team7.hrbank.domain.department.dto;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -11,7 +12,7 @@ public record DepartmentRequestDTO(
         @NotNull(message = "Description 필수")
         String description,
         @NotNull(message = "개설일은 필수")
-        @DefaultValue("2023-01-01")
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
         LocalDate establishmentDate
 ) {
 }
